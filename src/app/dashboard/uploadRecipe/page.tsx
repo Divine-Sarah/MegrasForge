b11d9ebc49520dc7402ede7bfd07feda15087194
Divine-Sarah <divine-sarah@Divine-Sarahs-MacBook-Pro.local>
2023-11-07 00:36:16 +0100

    (feat),implemented the uploadvideo

--- src/app/dashboard/uploadRecipe/page.tsx
+++ src/app/dashboard/uploadRecipe/page.tsx
@@ -9,52 +9,11 @@ import { useForm } from "react-hook-form";
 import { toast } from "react-toastify";
 
 
-const recipeSchema = z.object({
-  title: z.string().min(2, {
-    message: "Title must be at least 2 characters.",
-  }),
-  ingredients: z.string().min(2, {
-    message: "Ingredients must be at least 2 characters.",
-  }),
-  instructions: z.string().min(2, {
-    message: "Instructions must be at least 2 characters.",
-  }),
-  // Assuming you're storing image URL in the database, use z.string() for the image field
-  image: z.string().url({
-    message: "Invalid image URL format.",
-  }),
-});
 
 
-function uploadRecipe() {
-   const form = useForm<z.infer<typeof recipeSchema>>({
-     resolver: zodResolver(recipeSchema),
-     defaultValues: {
-       title: "",
-       ingredients: "",
-       instructions: "",
-       image: "",
-     },
-   });
 
-  // Define a submit handler.
+ function uploadRecipe() {
 
-   const onSubmit = (values: z.infer<typeof recipeSchema>) => {
-     // Assuming SERVICE.recipe.uploadRecipe is an async function that sends a request to save the recipe
-     SERVICE.recipe
-       .uploadRecipe(values)
-       .then((res) => {
-         console.log("response", res);
-         toast.success(res.data.message ?? "Recipe saved successfully");
-       })
-       .catch((err) => {
-         console.error(err);
-         toast.error(
-           err.response?.data?.message ??
-             "An error occurred while saving the recipe"
-         );
-       });
-   };
 
   return (
     <DashboardLayout>
@@ -62,8 +21,6 @@ function uploadRecipe() {
         <div className="items-center justify-center p-6 mx-auto ">
           <h2 className="mb-4 text-2xl font-semibold">Upload Recipe</h2>
           <form
-            {...form}
-            onSubmit={form.handleSubmit(onSubmit)}
             className="space-y-3"
           >
             <div className="mb-4">
@@ -121,4 +78,4 @@ function uploadRecipe() {
   
 }
 
-export default uploadRecipe;
+export default uploadRecipe
--- src/app/dashboard/uploadVideo/page.tsx
+++ src/app/dashboard/uploadVideo/page.tsx
@@ -1,13 +1,100 @@
-import React from 'react'
-import Link from 'next/link'
+"use client";
 
-export default function page() {
-  return (
+import DashboardLayout from "@/components/ui/layout/dashbaordLayout";
+import React, {useState} from "react";
+import axios from 'axios'
 
 
-    <div className='flex w-full justify-evenly'>
-      <Link href="/upload-button">Upload Button</Link>
-      <Link href="/upload-dnd">Upload Drag n Drop</Link>
-    </div>
-  )
+
+const UploadVideo =() =>{
+
+const [title, setTitle] = useState(null);
+const [description, setDescription] = useState(null);
+const [videoUrl, setVideoUrl] = useState(null);
+
+const handleTitleChange = (e:any) => {
+  setTitle(e.target.value);
+};
+const handleDescriptionChange = (e:any) => {
+  setDescription(e.target.value);
 }
+const handleVideoUrlChange = (e:any) => {
+  setVideoUrl(e.target.value);
+}
+
+const upload = () =>{
+  console.log('checking')
+  axios
+    .post("http://localhost:4000/api/v1/storeVideo", {
+      title,
+      description,
+      videoUrl
+    })
+    .then(function (response) {
+      console.log(response,'response');
+    })
+    .catch(function (error) {
+      console.log(error, 'error');
+    });
+}  
+  return (
+    <DashboardLayout>
+      <div className="w-full rounded-lg ">
+        <div className="items-center justify-center p-6 mx-auto ">
+          <h2 className="mb-4 text-2xl font-semibold">Upload Recipe</h2>
+          <form className="space-y-3">
+            <div className="mb-4">
+              <label className="block mb-1 text-sm font-medium text-gray-600">
+                Title:
+              </label>
+              <input
+                onChange={handleTitleChange}
+                type="text"
+                name="title"
+                className="w-full p-2 border rounded"
+                placeholder="Enter recipe title"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block mb-1 text-sm font-medium text-gray-600">
+                Description:
+              </label>
+              <textarea
+                onChange={handleDescriptionChange}
+                name="ingredients"
+                className="w-full p-2 border rounded"
+                placeholder="Enter description"
+              ></textarea>
+            </div>
+
+            <div className="mb-4">
+              <label className="block mb-1 text-sm font-medium text-gray-600">
+                Video:
+              </label>
+              <input
+                onChange={handleVideoUrlChange}
+                type="file"
+                accept="video/*"
+                className="border"
+              />
+            </div>
+            <button
+              onClick={() => {
+                upload();
+              }}
+              type="submit"
+              className="p-2 text-white rounded bg-[#BA80E6] hover:bg-blue-600"
+            >
+              Upload Video
+            </button>
+          </form>
+        </div>
+      </div>
+      <section>
+        <div>list of recipes bt title only with a delete button</div>
+      </section>
+    </DashboardLayout>
+  );
+}
+
+export default UploadVideo;
